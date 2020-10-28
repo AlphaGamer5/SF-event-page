861d65c5276c123f1a4d43925d739161d5f01680
Shivam Nayak <shivamnayak5apr@gmail.com>
2020-10-28 14:11:03 +0530

    Commit 04

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,8 @@
 import { Card } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import React from "react";
+import data from "../../json files/data.json";
+import './about.module.css'
 
 const useStyles = makeStyles({
   card:{
@@ -13,21 +15,37 @@ const useStyles = makeStyles({
   },
   h1:{
     marginBottom:"20px",
-  }
-})
+  },
+  p:{
 
+  }
+
+})
+let abouts = [{eventName:"Nukkad",id:0,about:''}];
+for (var key in data) {
+  if (data.hasOwnProperty(key)) {
+    abouts.push({eventName:key,about:data[key].about});
+    abouts.forEach((item, i) => {
+      item.id = i + 1;
+    }); 
+  }
+} 
+console.log(abouts)
+
+let specificAbout = abouts[1].about;
+
+console.log(specificAbout)
 const AboutPage = () => {
   const classes = useStyles();
-
   return(
-  <Card className={classes.card}>
-    <h1 className={classes.h1}>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non cupiditate labore, deleniti enim tenetur sed reiciendis recusandae.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non cupiditate labore, deleniti enim tenetur sed reiciendis recusandae.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non cupiditate labore, deleniti enim tenetur sed reiciendis recusandae.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non cupiditate labore, deleniti enim tenetur sed reiciendis recusandae.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non cupiditate labore, deleniti enim tenetur sed reiciendis recusandae.</p>
-  </Card>
+    <Card className={classes.card}>
+      <h1 className={classes.h1}>About</h1>
+      <ul>
+        {
+          <p>{specificAbout}</p>
+        }
+      </ul>
+    </Card>
 )};
 
 export default AboutPage;
